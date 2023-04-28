21d7ad53823d665fc0e23b9036a6afc23e2127d4
Ana Carolina dos Santos Rocha <78958795+annakks@users.noreply.github.com>
2023-04-28 14:38:10 -0300

    Fix: Init sreens Detail

---
+++ pro-manage-app/src/Screens/Detail/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styled.tsx';
+
+export default () => {
+    return (
+        <View style={styles.Container}>
+            <Text>Detail</Text>
+        </View>
+    )
+}
