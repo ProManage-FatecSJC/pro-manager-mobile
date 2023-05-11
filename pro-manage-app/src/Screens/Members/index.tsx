ad8853907c798d29aec13ffb2c82f2f395c4e0da
Ana Carolina dos Santos Rocha <78958795+annakks@users.noreply.github.com>
2023-05-11 10:52:03 -0300

    Feat: 1.14 - [MOBILE] LISTAGEM DE MEMBROS #29 finish

--- pro-manage-app/src/Screens/Members/index.tsx
+++ pro-manage-app/src/Screens/Members/index.tsx
@@ -5,13 +5,16 @@ import SearchBar from '../../components/SearchBar.tsx';
 import { SessionController } from '../../session/SessionController.ts';
 import api from '../../api/api.ts';
 import { URI } from '../../api/uri.ts';
+import ButtonGreen from '../../components/ButtonGreen.tsx';
+import CardDetailMembers from '../../components/CardDetailMembers.tsx';
 
 export default ({ navigation, route }: any) => {
 
   const { idProp } = route.params;
 
   const [id, setId] = useState(idProp);
-  const [members, setMembers] = useState([]);
+    const [membersData, setMembersData] = useState([])
+    const [members, setMembers] = useState([]);
   const [name, setName] = useState();
   const [fantasyName, setFantasyName] = useState();
   const [cnpj, setCnpj] = useState();
@@ -23,13 +26,14 @@ export default ({ navigation, route }: any) => {
     const token = await sessionController.getToken();
 
     try {
-      await api.get(URI.MEMBERSBYID +`/${id}`, {
+      await api.get(URI.MEMBERS +`/${id}`, {
           headers: {
             Authorization: token,
           },
         })
         .then((response) => {
-          setMembers(response.data);
+          setMembersData(response.data);
+          setMembers(membersData)
           console.log(members);
         });
     } catch (error) {
@@ -40,19 +44,55 @@ export default ({ navigation, route }: any) => {
   useEffect(() => {
     if (count < 2) {
       handleMembers();
+      console.log(count)
       setCount(count + 1)
     }
   }, [members]);
 
+  const handleSearch = (text: any) => {
+    if (!text) {
+      setMembers(membersData);
+      return;
+    }
+
+    const filteredMembers = membersData.filter((partner: any) => {
+      const name = partner.name.toLowerCase();
+      const searchTerm = text.toLowerCase();
+      return name.includes(searchTerm);
+    });
+
+    console.log("MEMBROS FILTRADOS: ", filteredMembers);
+
+    setMembers(filteredMembers);
+  };
+
+
   return (
     <View style={styles.Container}>
       <Text style={styles.Text1}> Membros</Text>
-      <SearchBar placeholder={"Pesquisa"} />
+      <SearchBar placeholder={"Pesquisa"} onChangeText={handleSearch} />
       <ScrollView>
         <View>
-          
+          {members.map((item: any) => (
+            <CardDetailMembers
+              key={item.id}
+              name={item.name}
+              nameFantasy={item.trade_name}
+              cnpj={item.CNPJ}
+              onPress={() => {}}
+            />
+          ))}
         </View>
       </ScrollView>
+      <ButtonGreen
+        title={"Cadastrar Membro"}
+        onPress={() => {
+          console.log(idProp);
+          navigation.navigate("Members", {
+            idProp: id,
+          });
+        }}
+      />
     </View>
   );
 };
