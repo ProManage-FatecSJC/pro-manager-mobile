b330f422069e164b3f6b0ff499761774af970754
Ana Carolina dos Santos Rocha <78958795+annakks@users.noreply.github.com>
2023-05-22 17:29:47 -0300

    Fix: CADASTRO DE USUÁRIOS #35

--- pro-manage-app/src/Screens/UserRegister/index.tsx
+++ pro-manage-app/src/Screens/UserRegister/index.tsx
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
 import { 
   View, 
-  Text, 
-  TextInput,
+  Text,
 } from 'react-native';
-import Button from "../../components/ButtonAddUser.tsx";
 import { useNavigation } from "@react-navigation/native"; 
 import styles from './styles.tsx';
-import ButtonCancel from "../../components/ButtonRed.tsx";
 import SignInServeral from "../../components/SignInSeveral.tsx";
 import { SessionController } from "../../session/SessionController.ts";
 import api from "../../api/api.ts";
 import { URI } from "../../api/uri.ts";
+import SignInInput from "../../components/SignInput.tsx";
+import ButtonBlue from "../../components/ButtonBlue.tsx";
+import ButtonRed from "../../components/ButtonRed.tsx";
+import PartnerSignIn from "../../components/PartnerSignIn.tsx";
 
 export default ({navigation}: any) => {
 
-  const [UserName, setUserName] = useState('');
-  const [UserEmail, setUserEmail] = useState('');
-  const [UserPass, setUserPass] = useState('');
-  const [UserRole, setUserRole] = useState('');
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userPass, setUserPass] = useState('');
+  const [userRole, setUserRole] = useState(1);
 
 const sessionController = new SessionController()
 
@@ -26,14 +27,14 @@ const sessionController = new SessionController()
   const token = await sessionController.getToken()
    console.log(user)
    await api.
-  post(URI.USER, user, {
+  post(URI.REGISTER, user, {
        headers: {
            Authorization: token
        }
    })
    .then(response => {
        if(response.status == 200){
-            navigation.navigate('profile')
+            navigation.navigate("Profile");
        }
    })
   .catch(error => {
@@ -41,52 +42,71 @@ const sessionController = new SessionController()
    })
  }
 
-const RoleOptions = [
-  { label: 'Administrador', value: 0 },
-  { label: 'Observador', value: 1 },
-  
-];
+const RoleOptions = ["Administrador", "Observador"];
+
+const handleSelect = (selectedOption: any) => {
+  if(selectedOption == "Administrador"){
+    setUserRole(0)
+  } else {
+    setUserRole(1);
+  }
+  ;}
+
 
 let user = {
-  name: UserName,
-  email: UserEmail,
-  senha: UserPass,
-  Role: parseInt(UserRole)
-}
-
-const handleSelect = (selectedOption: string) => {
-      console.log(`Função Selecionada: ${selectedOption}`);
-    };
+  name: userName,
+  email: userEmail,
+  password: userPass,
+  role: userRole,
+};
 
 const onCancel = () => {
       console.log(`Cancelado`);
-      navigation.navigate('profile')
+      navigation.navigate("Profile");
     };
 
 
     return (
-    <View style={styles.Title}>
-      <Text style={styles.Text1}>Adicionar um usuário</Text>
-      <Text style={styles.Text2}>Adicione um usuário ao sistema</Text>
-      <View style={styles.Divider}></View>
-         
-        <Text style={styles.Text}>Nome do Usuário</Text>
-        <TextInput style={styles.input} placeholder="Digite o nome do usuário " onChangeText={setUserName} value={UserName}/>
+      <View style={styles.Container}>
+        <View>
+          <Text style={styles.Text1}>Adicionar um usuário</Text>
+          <Text style={styles.Text2}>Adicione um usuário ao sistema</Text>
+          <View style={styles.Divider}></View>
 
-        <Text style={styles.Text}>Email</Text>
-        <TextInput style={styles.input} placeholder="Digite o Email" onChangeText={setUserEmail} value={UserEmail}/>
+          <Text style={styles.Text}>Nome do Usuário</Text>
+          <PartnerSignIn
+            placeholder={"Digite o Nome"}
+            onChangeText={setUserName}
+            value={userName}
+          />
 
-        <Text style={styles.Text}>Senha</Text>    
-        <TextInput style={styles.input} placeholder="Digite a senha" onChangeText={setUserPass} secureTextEntry={true} value={UserPass}/>
+          <Text style={styles.Text}>Email</Text>
+          <PartnerSignIn
+            placeholder={"Digite o Email"}
+            onChangeText={setUserEmail}
+            value={userEmail}
+          />
 
-        <SignInServeral options={RoleOptions} onSelect={handleSelect} /> 
-          
-          <Button title="Salvar" onPress={function (): void {
-            throw new Error("Function not implemented.");
-          }} />
-          
-          <ButtonCancel title="Cancelar" onPress={onCancel}/>
-          
+          <Text style={styles.Text}>Senha</Text>
+          <PartnerSignIn
+            placeholder={"Digite a senha"}
+            onChangeText={setUserPass}
+            value={userPass}
+
+          />
+
+          <Text style={styles.Text}>Nível de Acesso</Text>
+          <SignInServeral options={RoleOptions} onSelect={handleSelect} />
+
+          <ButtonBlue
+            title={"Salvar"}
+            onPress={() => {
+              handleNewUser()
+            }}
+          />
+
+          <ButtonRed title="Cancelar" onPress={onCancel} />
         </View>
-      );
-    }
+      </View>
+    );
+}
